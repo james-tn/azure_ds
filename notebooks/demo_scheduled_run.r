1e3f01c1b02a4911395c46a4e453579b214b4c9f
James Nguyen <james.n@ischool.berkeley.edu>
2018-04-12 06:37:39 -0700

    first commit

---
+++ notebooks/demo_scheduled_run.r
@@ -0,0 +1,10 @@
+# Databricks notebook source
+
+library(SparkR)
+df <- read.df("/mnt/demo/csv/201501-hubway-tripdata.csv",
+                    source = "csv", header="true", inferSchema = "true")
+df$duration_hours <- df$tripduration / 60
+
+df <- select(df, "duration_hours","starttime")
+df <- filter(df, df$duration_hours >2)
+write.df(df, path="dbfs:/mnt/demo/csv/duration.parquet", source="parquet", mode="overwrite")
