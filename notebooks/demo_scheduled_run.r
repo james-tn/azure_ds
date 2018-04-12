74fe0511147fdf37ae276ef26fd6d2a8fa0a7bce
James Nguyen <james.n@ischool.berkeley.edu>
2018-04-12 12:10:41 -0700

    adding ODBC

--- notebooks/demo_scheduled_run.r
+++ notebooks/demo_scheduled_run.r
@@ -1,4 +1,27 @@
 # Databricks notebook source
+library(sparklyr)
+
+require(RODBC)
+
+# establish a connection using the DSN you created earlier
+conn <- odbcConnect("databricks")
+
+# run a SQL query using the connection you created
+res <- sqlQuery(conn, "SELECT * FROM fm_12_tbl")
+
+# print out the column names in the query output
+names(res) 
+
+# print out the number of rows in the query output
+nrow (res)
+
+res <- sqlQuery(conn, "CREATE table new_tbl as (SELECT * FROM fm_12_tbl)")
+
+
+
+
+# COMMAND ----------
+
 library(SparkR)
 df <- read.df("/mnt/demo/csv/201501-hubway-tripdata.csv",
                     source = "csv", header="true", inferSchema = "true")
