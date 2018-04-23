550925f086265f3545791a7faa6f8a8664f2aa30
james-tn <james.n@ischool.berkeley.edu>
2018-04-23 03:10:55 +0000

    commit

--- notebooks/demo_scheduled_run.r
+++ notebooks/demo_scheduled_run.r
@@ -1,3 +1,29 @@
+library(sparklyr)
+
+require(RODBC)
+
+# establish a connection using the DSN you created earlier
+conn <- odbcConnect("databricks")
+
+# run a SQL query using the connection you created
+res <- sqlQuery(conn, "SELECT * FROM fm_12_tbl")
+
+# print out the column names in the query output
+names(res) 
+
+# print out the number of rows in the query output
+nrow (res)
+
+res <- sqlQuery(conn, "CREATE table new_tbl as (SELECT * FROM fm_12_tbl)")
+
+
+
+
+
+
+
+
+
 # Databricks notebook source
 library(SparkR)
 df <- read.df("/mnt/demo/csv/201501-hubway-tripdata.csv",
